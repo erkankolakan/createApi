da7e925d1a4b7abe78ce157b67c8aa8f31f2a6f8
Erkan <erkankolakan@gmail.com>
2023-09-26 16:31:58 +0300

    kayıt güncelleme işlemi

--- routes/products.js
+++ routes/products.js
@@ -17,35 +17,8 @@ const {Product , validateProduct} = require("../models/product")
  */
 
 router.get("/", async(req ,res) => {
-    // const products = await Product.find() 
-    // const products = await Product.fing({price : {$ne : 10000}}) // price != 10000 olmayan tüm öğeleri al
-    // const products = await Product.fing({price : {$gt : 10000}}) // price > 10000  olan tüm öğeleri al
-    // const products = await Product.fing({price : {$gte : 10000}})//price >= 10000  olan tüm öğeleri al
-    // const products = await Product.fing({price : {$lt : 10000}}) // price < 10000  olan tüm öğeleri al
-    // const products = await Product.fing({price : {$lte : 10000}}) // price <= 10000  olan tüm öğeleri al
-    // const products = await Product.fing({price : {$in :[10000 ,20000]}}) // fiyatı 10000 ile 20000 eşit olan öğeleri al
-    // const products = await Product.fing({price : {$nin :[10000 ,20000]}}) // fiyatı 10000 ile 20000 e eşit olmayan öğeleri al
-    // const products = await Product.fing({price : {$gte : 10000 , $lte: 20000 }}) // price >= 10000 && price <= 20000
-    // const products = await Product.fing({price : {$gte : 10000 , $lte: 20000 } , name: "Iphone" }) // Bu şekilde yazarak fiyatı 10000 ile 20000 arasında olan ve name i Iphone olan öğeleri al. Burada aslında AND operatoru kullanmış oluyoruz
-/*  const products = await Product.fing()
-                                .or([
-                                    { price : {$gte : 10000 }}, //1.kriter
-                                    {isActive: true } //2. kriter
-                                ]) // or demek içinde yani yada demek içinde bu şekilde kullanmamız gerekir or.([]) köşeli parantez kullanmayı unutma. Fiyatı (10000 >= or isActive== true) olan değerleri al */
+    const products = await Product.find() 
 
-// startwith --> herhangi bir bilgiyle başlıyor mu ?
-
-// const products = await Product.find({ name: /^iphone/}i) //sonu önemli değil, iphone ile başlayan tüm öpeleri al 
-
-// end with --> herhangi bir bilgiyle bitiyor mu ?
-
-// const products = await Product.find({name: /iphone$/}i) //başlangıcı önemli değil sonu iphone ile başlayan tüm öğeleri al.
-
-// contains
-
-    const products = await Product.find({name: /.*iphone.*/i}) //içerisinde iphone geçen tüm öğeleri al !! büyük küçük harf önemli olmasın diyorsanız sonuna i eklememiz gerekir. i eklemezsek girilen kelimede büyük küçük harfe bakar.
-
- 
     res.send(products)
 })
 
@@ -75,22 +48,59 @@ router.post("/", async (req ,res) => {
     })
   
 
+
+/* 1. yol -- query first - findbtid ile öğe alnır ve yeni değerler verilerek save() edilir
+   2. yol -- update diyerek tek işlemde güncelleyebiliriz */
+
 router.put("/:id", async(req ,res) =>{
+
+    //1. parametre öğe seçme işlemi, 2. parametrede ise hangi alanları set etmek istiyorzu yanii güncellemek istiyorsunuz.
+
     const id = req.params.id
-    const product = await Product.findById({_id:id});
-    if (!product) {
-        return res.status(400).send("aradığınız ürün bulunamadı")
-    }
-    const {error} = validateProduct(req.body)   
 
-    if ( error ) { 
-        return res.status(400).send(result.error.details[0].message)
-    }
+    const product = await Product.findByIdAndUpdate({_id: id},{
+            $set:{
+                name : req.body.name,
+                price : req.body.price,
+                description : req.body.description,
+                imgUrl : req.body.imgUrl,
+                isActive : req.body.isActive,
+            }
+        },{new:true});
 
-   product.name = req.body.name; 
-   product.price = req.body.price;
-   res.send(product)
-})
+        res.send(product);
+    /* burada result bize update edilen kayıt sayısını veririr. Ama bu öğeyi güncelledi gerçeğini değiştirmiyor.
+        Bunun güzel yani öğeyi seçiyim sonra gidiyim güncelliyim diye birşey yok tek sorguda seçip güncelliyoruz.
+        Sadece id ile alakalı değil istersek isActive si true olantüm ürünleri güncelle diyebiliriz
+
+        !!! updateOne demek yerine findByIdAndUpdate diyerek güncellemeden önceki değerleri alabiliriz 
+        !!! yine findByIdAndUpdate kullanarak ve 3. parametreye {new: true} diyerek güncellemeden sonraki değeri alabiliriz
+    */
+
+
+//     const id = req.params.id
+//     const product = await Product.findById({_id:id});
+
+
+//     if (!product) {
+//         return res.status(400).send("aradığınız ürün bulunamadı")
+//     }
+//     const {error} = validateProduct(req.body)   
+
+//     if ( error ) { 
+//         return res.status(400).send(result.error.details[0].message)
+//     }
+
+//    product.name = req.body.name; 
+//    product.price = req.body.price;
+//    product.description = req.body.description;
+//    product.imgUrl = req.body.imgUrl;
+//    product.isActive = req.body.isActive;
+//    res.send(product)
+
+//    const updatedProduct = await product.save()
+//    res.send(updatedProduct)
+});
 
 
 router.delete("/:id" , async(req, res) => {
