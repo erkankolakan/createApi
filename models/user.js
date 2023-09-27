116fa01bdc7cc6b9b583b4c6d8d7f0a5add39d2b
Erkan <erkankolakan@gmail.com>
2023-09-27 20:23:28 +0300

    Auhenticating Users

--- models/user.js
+++ models/user.js
@@ -18,10 +18,20 @@ const userSchema = mongoose.Schema({
     }
 }, {timestamps: true})
 
-  //validate
-  const validateUser = (user) => { 
+  //User oluşturken ihtiyacımız olan 
+  const validateRegister = (user) => { 
     const schema = new Joi.object({
         name: Joi.string().min(3).max(50).required(), 
+        email: Joi.string().min(3).max(50).required().email(), 
+        password: Joi.string().min(5).required(), 
+
+    })
+    return schema.validate(user) //user den gelen bilgileri schema ya göre validate et
+}
+
+  //User oluşturken ihtiyacımız olan 
+  const validateLogin = (user) => { 
+    const schema = new Joi.object({
         email: Joi.string().min(3).max(50).required().email(), 
         password: Joi.string().min(5).required(), 
 
@@ -32,4 +42,4 @@ const userSchema = mongoose.Schema({
 const User = mongoose.model("User" , userSchema)
 
 
-module.exports = { User , validateUser }
+module.exports = { User , validateRegister, validateLogin }
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const router = express.Router()
-const {User ,validateUser } = require("../models/user")
+const {User ,validateRegister, validateLogin } = require("../models/user")
 const bcrypt = require("bcrypt")
 
 
@@ -11,7 +11,7 @@ router.get("/" , (req, res) => {
 
 
 router.post("/" , async(req, res) => {
-    const {error} = validateUser(req.body);
+    const {error} = validateRegister(req.body);
     if (error) {
         return res.status(400).send(error.details[0].message);
     }
@@ -36,6 +36,26 @@ router.post("/" , async(req, res) => {
     res.send(user)
 
 })
+
+router.post("/auth", async (req, res) => {
+    const {error} = validateLogin(req.body) //validate loginine bilgileri gönderip kontorl ettiriyoruz.
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+
+    let user = await User.findOne({email : req.body.email})
+
+    if (!user) {
+        return res.status(400).send("hatalı email veya parola")
+    }
+
+    const isSuccess = await bcrypt.compare(req.body.password, user.password ) //gelen şifre ile userdaki şifreyi karşılaştır dedik
+
+    if (!isSuccess) {
+        return res.status(400).send("hatalı parola")
+    }
+    res.send(true)
+} )
 
 
 module.exports = router
