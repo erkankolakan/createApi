6581936a79ea2e481ffdceffc2a570a48cd47948
Erkan <erkankolakan@gmail.com>
2023-09-27 11:51:46 +0300

    (ilişki türkeri) reference and embedded documents

--- models/category.js
+++ models/category.js
@@ -18,3 +18,57 @@ const Category = mongoose.model("Category" , categorySchema)
 
 
 module.exports = {Category , validateCategory }
+
+
+
+/*
+İLİŞKİ TÜRLERİ
+
+** reference -- referans tutmak aslında SQL yapsındaki yabancı anahtar kavramına benziyor ve bilgilerin ayrı bir döküman içinde saklanması.
+ör/
+
+var egitmen = {
+    id: 1,
+    name: "Erkan"
+}
+
+
+var kurs = {
+    title: "node.js",
+    egitmen: "reference" -- burda referans vermemiz gerek örneğin node.js dersini sadık veriyorsa 1 no lu id
+}
+
+-----------------------------------------------------------------------------------
+** embedded documents -- ilişkileri aynı döküman içersinde saklayabiliriz. 
+ör/
+
+    var kurs = {
+        title: "aspp.net",
+        egitmen: {              -- bir döküman içinde farklı bir döküman saklanmış oluyor
+            name: "Erkan"
+        }
+    }
+
+-----------------------------------------------------------------------------------
+** hybrid -- her ikisinide kullandığımız karışık sistem.
+ör/
+
+    var product = {
+        name: "samsung s22",
+        price: 20000,               //--> güncell fiyat
+        desc: ""
+    }
+
+    var order = {
+        id :1,
+        date: "",
+        product:{
+            name: "samsung s22",          
+            price:  19000,          //--> kullanıcı sipariş verdiği tarihteki ürün fiyatı. Sonuçta sonra dan fiyat artıyor
+
+        }
+    }
+product bilgisindeki tüm bilgileri getirmeye gerek yok en çok kullanılan bilgileri almamız yeterli. Eğer product üzerinden başka bir bilgiye ihtiyacımız olursada order a  vermiş olduğumuz product referansından ulaşabiliriz. Yani ben istediğim zaman productın detayına gitmek istediğim zaman id yi kullanım detayına gidebilirim Bu sayede extra bir sorgu yazmak zorunda kalmayız. Order üzerinden ürün ismi ve fiyatına da zaten direk ulaşabiliyoruz
+
+*/ 
+
