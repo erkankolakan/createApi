381840ec0b8589d680ad5077e4c94116ace7f6bb
Erkan <erkankolakan@gmail.com>
2023-09-28 15:24:34 +0300

    logging errors in mongodb hataları mongogbe gönder

--- middleware/logger.js
+++ middleware/logger.js
@@ -1,21 +1,29 @@
 const {transports , format, createLogger} = require("winston")
 const {combine, timestamp, prettyPrint} = format
+require("winston-mongodb")
+
+
+const userName = "erkankolakan";
+const password = "tmVuxo01HxJRuSz2";
+const dataBase = "shopdb";
 
 const logger = createLogger({
-    level: "debug", //-> buraya error info gibi leveller de yazılabilir.
-    // format: winston.format.json(), //-> bize json tipinde default bir mesaj gönderir.
-
-    // zaman bilgisini veya farklı özellikler eklemek istersen aşağıdaki gibi bir format yazmalısın
+    level: "debug", //defaul olarak debug modda çalışacaklar
     format: combine (
-        timestamp({
-            format: "MM-DD-YYYY HH:mm:ss"
-        }),
-        prettyPrint() //daha düzgün bir şekilde her gelen hatayı alt satıra taşır
+        timestamp({format: "MM-DD-YYYY HH:mm:ss"}),
+        prettyPrint()
     ),
-
-    transports:[ //en önemli kısmı budur bir kaçtane ortama bu kaydı yapabiliyoruz
-        new transports.Console(), //-> console ekranına çıktı alıcaz
-        new transports.File({filename: "logs.log"}) //-> dosya ismini yazdık dosayayı kendisi oluşturuyır ve içine gelen hataları not alıyor
+    transports:[
+        new transports.Console(), 
+        new transports.File({filename: "logs.log" , level: "error"}), // sadece error mesajşarını dosyaya kaydet
+        new transports.MongoDB({
+            level: "error", //-> hangi hatalar veri tabanına kayıt olsun. error etiketi olanlar kayıt olsun. Ayrıca biz burada yukardaki debug bilgisini ezerek normal servis ettiğimiz zamanda hataların databaseye kaydolmasını söyledik ama Console fln onlar sadece debug modda çalışır. İstediğimiz yerde üsteki yazan debug bilgisini ezebiliriz
+            db: `mongodb+srv://${userName}:${password}@cluster0.shuzdaz.mongodb.net/${dataBase}?retryWrites=true&w=majority`,
+            options:{
+                useUnifiedTopology: true
+            },
+            collection:"server_logs" //-> verir tabanında server_log isminde kaydolucak bu bilgiler
+        })
     ]
 })
 
