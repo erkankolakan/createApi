fae9c5506c79fdb143a0d06b37b0028798a7d340
Erkan <erkankolakan@gmail.com>
2023-09-30 14:31:37 +0300

    projeyi yayımlama ayarları

--- app.js
+++ app.js
@@ -5,15 +5,21 @@ require("./startup/logger")
 
 require("./startup/routes")(app) //-> fonksiyonu çağırıyoruz ve ihtiyacı olan parametreyi gönderiyoruz
 require("./startup/db")()
+if (app.get("env") == "production")  { //uygulmamanın yayımlandığı yer production sa bu paketleri kullan
+    require("./startup/production")(app)
+}
 
-
-// app.use(cors({
-//     origin:"*",  
-//     methods:["GET" , "POST"]
-// })) 
+/*
+app.get("env")  ile process.env.NODE_ENV aynı işleve yarar çevresel değişkenine uygulamanın çalıştığı ortamı (çevresi) döndüren bir yöntemdir.
+*/
 
 const port = process.env.PORT || 3000;
 
 app.listen(port , () => {
     console.log(`listening on port ${port}`)
-})  
+})
+
+// app.use(cors({
+// origin:"*",  
+// methods:["GET" , "POST"]
+// })) 
